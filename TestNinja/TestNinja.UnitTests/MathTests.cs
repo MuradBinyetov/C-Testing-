bdb1e6824f2f57560936f86341fd2c29a8eca22b
muradbinyetov <muradbinyetov@gmail.com>
2022-11-28 07:57:01 +0400

    Add Parametrized Test method(Generic method);

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -24,34 +24,37 @@ namespace TestNinja.UnitTests
         public void Add_WhenCalled_ReturnSumOfArguments()
         { 
             var result = _math.Add(1, 3);
-
+                
             Assert.That(result, Is.EqualTo(4));
         }
 
         [Test]
-        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        [TestCase(1,2,2)]
+        [TestCase(3,1,3)]
+        [TestCase(5,5,5)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a,int b, int expected)
         {  
-            var result = _math.Max(5, 3);
+            var result = _math.Max(a, b);
 
-            Assert.That(result,  Is.EqualTo(5));
+            Assert.That(result,  Is.EqualTo(expected));
         }
 
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
-        {  
-            var result = _math.Max(4, 8);
+        //[Test]
+        //public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        //{  
+        //    var result = _math.Max(4, 8);
 
-            Assert.That(result, Is.EqualTo(8));
-        }
+        //    Assert.That(result, Is.EqualTo(8));
+        //}
 
 
-        [Test]
-        public void Max_ArgumentAreEqual_ReturnTheSameArgument()
-        { 
-            var result = _math.Max(4, 4);
+        //[Test]
+        //public void Max_ArgumentAreEqual_ReturnTheSameArgument()
+        //{ 
+        //    var result = _math.Max(4, 4);
 
-            Assert.That(result, Is.EqualTo(4));
-        }
+        //    Assert.That(result, Is.EqualTo(4));
+        //}
 
     }
 }
